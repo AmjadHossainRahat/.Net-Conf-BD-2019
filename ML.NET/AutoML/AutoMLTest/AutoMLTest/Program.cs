bd7416643d4a8bacb0fbfc03af78f73ca7c09e85
amjad.hossain <amjad.hossain@selise.ch>
2019-10-27 17:39:06 +0600

    AutoML and MLCLI project added

---
+++ ML.NET/AutoML/AutoMLTest/AutoMLTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using AutoMLTestML.Model;
+using System;
+
+namespace AutoMLTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ML.NET World!");
+
+            // Add input data
+            var input = new ModelInput();
+            input.SentimentText = "ML.net is good but not the best";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine($"Prediction: {result.Prediction}, Score: {result.Score}");
+            Console.ReadKey();
+        }
+    }
+}
